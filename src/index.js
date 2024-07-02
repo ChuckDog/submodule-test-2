de410067146fa10069fd9e548542edc5fc36f68c
ChuckDog <xuzhong2012@my.fit.edu>
2024-07-02 11:10:22 +0800

    feat: call and export randomInt

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import getRandomInt from "./randomInt";
+
+const randomInt = getRandomInt(1, 1000);
+
+export default { randomInt, getRandomInt };
--- utils/helper.js
+++ utils/helper.js
@@ -1,2 +1,2 @@
-const getRandomInt = (min, max) =>
+export const getRandomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
